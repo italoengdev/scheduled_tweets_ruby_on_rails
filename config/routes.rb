9c4625f137f9051d514670b8cca0740f108bc637
italoengdev <italo_saraiva182@hotmail.com>
2023-03-15 16:45:35 -0300

    class 17

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,11 @@ Rails.application.routes.draw do
 
   post "sign_up", to: "registrations#create"
 
+  get "sign_in", to: "sessions#new"
+
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   root to:"main#index"
 end
