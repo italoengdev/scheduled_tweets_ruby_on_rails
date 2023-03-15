1f2fbce766e2cd9ab8ee34d951bae97e9f0daf18
italoengdev <italo_saraiva182@hotmail.com>
2023-03-15 11:35:42 -0300

    aula 15- creating user

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
 
-  post "sign_up", to: "registations#create"
+  post "sign_up", to: "registrations#create"
 
   root to:"main#index"
 end
