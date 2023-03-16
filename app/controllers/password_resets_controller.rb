a028497951f6fb69da9c76b719b34ebed5dfa694
italoengdev <italo_saraiva182@hotmail.com>
2023-03-16 11:31:07 -0300

    aula 22

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,5 +12,24 @@ class PasswordResetsController < ApplicationController
     end
     redirect_to root_path, notice:"If an account with this email was found, we have sent a link to reset your password"
   end
- 
+  def edit 
+    @user = User.find_signed!(params[:token],purpose:"password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert:"Your token has expired. Please Try Again"
+  end
+  def update
+    @user = User.find_signed!(params[:token],purpose:"password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, alert:"Your password was reset successfully. Please Sign In"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password,:password_confirmation)
+    
+  end
 end
