e76c3c6651b36219b5fe2e2e2c519426977ce0a4
italoengdev <italo_saraiva182@hotmail.com>
2023-03-16 00:38:34 -0300

    class 20 password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
   get "about", to:"about#index"
 
-  # Para manuntenção em escalabilidade do site URL Helpers get "about-us", to:"about#index", as: :about
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
 
   get "sign_up", to: "registrations#new"
-
   post "sign_up", to: "registrations#create"
 
   get "sign_in", to: "sessions#new"
-
   post "sign_in", to: "sessions#create"
 
   delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
 
   root to:"main#index"
 end
